91ad98f61bd17faf41e0f269174172301ddc5a4b
Muhammad Nauman <mhmmd.nauman@gmail.com>
2019-05-28 12:29:56 +0500

    roll number enrollment

--- DB/mydb.sql
+++ DB/mydb.sql
@@ -1,15 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.0
--- https://www.phpmyadmin.net/
+-- version 4.5.1
+-- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: May 22, 2019 at 09:21 AM
--- Server version: 10.1.25-MariaDB
--- PHP Version: 7.1.7
+-- Generation Time: May 28, 2019 at 09:29 AM
+-- Server version: 10.1.10-MariaDB
+-- PHP Version: 7.0.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -19,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `mydb`
+-- Database: `MyDb`
 --
 
 -- --------------------------------------------------------
@@ -29,34 +27,21 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `enrollment` (
-  `session` varchar(30) NOT NULL,
-  `year` int(30) NOT NULL,
-  `faculty` varchar(30) NOT NULL,
-  `faculty_id` int(30) NOT NULL,
-  `campus` varchar(30) NOT NULL,
-  `degree` varchar(30) NOT NULL,
-  `roll_no` int(6) UNSIGNED NOT NULL
+  `id` int(11) NOT NULL,
+  `p_code` varchar(10) DEFAULT NULL,
+  `rollno` varchar(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `enrollment`
 --
 
-INSERT INTO `enrollment` (`session`, `year`, `faculty`, `faculty_id`, `campus`, `degree`, `roll_no`) VALUES
-('IUI', 18, 'M', 2, 'A', 'B', 12),
-('IUI', 18, 'M', 2, 'A', 'B', 13),
-('IUI', 18, 'M', 2, 'A', 'B', 14),
-('IUI', 18, 'M', 2, 'A', 'B', 15),
-('IUI', 18, 'M', 2, 'A', 'B', 26),
-('fa', 2016, 'M', 0, 'b', 'A', 27),
-('A', 18, 'A', 0, 'V', 'MS', 28),
-('A', 18, 'A', 0, 'V', 'MS', 29),
-('A', 2016, 'M', 0, 'b', 'MS', 30),
-('A', 2016, 'M', 0, 'b', 'MS', 31),
-('iui', 16, 'A', 1, 'A', 'MS', 32),
-('iui', 16, 'A', 1, 'A', 'MS', 33),
-('iui', 18, 'M', 4, 'D', 'MS', 34),
-('sp', 2016, 'a', 4, 'A', 'B', 35);
+INSERT INTO `enrollment` (`id`, `p_code`, `rollno`) VALUES
+(1, 'IU16M2BA', 'IU16M2BA001'),
+(2, 'IU16M2BA', 'IU16M2BA002'),
+(3, 'IU16M2BA', 'IU16M2BA003'),
+(4, 'IU17M2BA', 'IU17M2BA001'),
+(5, 'IU17M1BA', 'IU17M1BA001');
 
 --
 -- Indexes for dumped tables
@@ -66,7 +51,7 @@ INSERT INTO `enrollment` (`session`, `year`, `faculty`, `faculty_id`, `campus`, 
 -- Indexes for table `enrollment`
 --
 ALTER TABLE `enrollment`
-  ADD PRIMARY KEY (`roll_no`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -76,8 +61,7 @@ ALTER TABLE `enrollment`
 -- AUTO_INCREMENT for table `enrollment`
 --
 ALTER TABLE `enrollment`
-  MODIFY `roll_no` int(6) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;COMMIT;
-
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- index1.php
+++ index1.php
@@ -1,13 +1,13 @@
 <?php
 
-$link = mysqli_connect("localhost", "root", "", "MyDb");
- 
+define("SERVER", "localhost");
+define("USER", "root");
+define("PASSWORD", "");
+define("DB", "MyDb");
 
-if($link === false){
-    die("ERROR: Could not connect. " . mysqli_connect_error());
-}
- 
-
+$conn = mysqli_connect(SERVER, USER, PASSWORD) or die ('Error connecting to mysql'); 
+mysqli_select_db($conn, DB)or die ('Error selecting to db');
+$link = $conn;
 $session = mysqli_real_escape_string($link, $_REQUEST['session']);
 $year = mysqli_real_escape_string($link, $_REQUEST['year']);
 $faculty = mysqli_real_escape_string($link, $_REQUEST['faculty']);
@@ -15,13 +15,37 @@ $faculty_id = mysqli_real_escape_string($link, $_REQUEST['faculty_id']);
 $campus = mysqli_real_escape_string($link, $_REQUEST['campus']);
 $degree = mysqli_real_escape_string($link, $_REQUEST['degree']);
 
- $sql = "INSERT INTO enrollment (session,year,faculty,faculty_id,campus,degree) VALUES ('$session','$year','$faculty','$faculty_id','$campus','$degree')";
-if(mysqli_query($link, $sql)){
-    echo "Records added successfully.";
-} else{
-    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
-}
- 
+$p_code=$session.$year.$faculty.$faculty_id.$campus.$degree;
 
-mysqli_close($link);
+ $query="SELECT * FROM enrollment where p_code = '$p_code' ;";
+
+//IU16M2BA001
+$result = $conn->query($query) or die($conn->error.__LINE__);
+$arr = array();
+if($result->num_rows > 0) {
+    
+    $query_last="SELECT * FROM enrollment where p_code = '$p_code' ORDER BY `enrollment`.`rollno` DESC;";
+    $result_last = $conn->query($query_last) or die($conn->error.__LINE__);
+    $row_last = $result_last->fetch_assoc();
+    $rollno = $row_last['rollno'];
+     
+    $lastpart = substr($rollno,8,3);
+    $lastpart++;
+    if($lastpart < 10){
+        $lastpart = "00".$lastpart;
+    }elseif($lastpart < 100){
+        $lastpart = "0".$lastpart;
+    }
+    $R=$p_code.$lastpart;
+    $sql = "INSERT INTO `enrollment` (`id`,`p_code`, `rollno`) VALUES (NULL,'$p_code', '$R')";
+    $conn->query($sql);
+       
+ }else{
+    $lastpart = "001";
+    $R=$p_code.$lastpart;
+    $sql = "INSERT INTO `enrollment` (`id`,`p_code`, `rollno`) VALUES (NULL,'$p_code', '$R')";
+    $conn->query($sql);
+ }
+
+
 ?>
